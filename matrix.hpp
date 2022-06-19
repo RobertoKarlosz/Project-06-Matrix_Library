30e61540221bf62711ad704b7380fe51610919d3
Robi <roberto.kr0114@gmail.com>
2022-06-19 20:56:09 +0200

    Class definition

---
+++ matrix.hpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+class Matrix {
+    size_t rows;
+    size-t cols;
+    vector<T> values;
+public:
+    Matrix() {}
+    Matrix() {}
+
+    // Getters
+
+    size_t getRows() {}
+    size_t getCols() {}
+
+    // Operators for Matrix operations
+
+    Matrix<T> operator-() {}
+    Matrix<T> operator-() {}
+    Matrix<T> operator+() {}
+    Matrix<T> operator*() {}
+    Matrix<T> operator*() {}
+    Matrix<T> operator/() {}
+    Matrix<T> operator/() {}
+
+    void operator+=() {}
+    void operator-=() {}
+    void operator*=() {}
+    void operator/=() {}
+
+    // Special operands
+
+    Matrix<T> transpose() {}
+    Matrix<T> multiplyElements() {}
+};
